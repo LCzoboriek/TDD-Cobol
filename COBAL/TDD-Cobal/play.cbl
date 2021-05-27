ca42fb680c1623deca713675f4d432040ad9c6f7
Luke Czoboriek <LC169782@protonmail.com>
2021-05-27 12:18:57 +0100

    Pronouns

--- COBAL/TDD-Cobal/play.cbl
+++
@@ -1,8 +0,0 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. "My Celebrator".
-       PROCEDURE DIVISION.
-           DISPLAY "I'm alright though, it's pretty cool this COBOL" 
-           " stuff isn't it. I'm not sure what the weather is going"
-           " to be like tomorrow though?".
-           
-
