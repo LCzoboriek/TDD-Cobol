12eb2e1735a1300e044f1a952a1aee59b6bfa5b5
Luke <lc169782@protonmail.com>
2021-05-28 14:11:03 +0100

    Added main program functionality

--- CUSTOMERS/main-program.cbl
+++ CUSTOMERS/main-program.cbl
@@ -1,5 +1,17 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. main-program.
        DATA DIVISION.
+           WORKING-STORAGE SECTION.
+           01 WS-TODAY PIC X(5).
+           01 WS-TODAY-DAY PIC 99.
+           01 WS-TODAY-MONTH PIC 99.
+
        PROCEDURE DIVISION.
+           MOVE FUNCTION CURRENT-DATE(5:2) TO WS-TODAY-MONTH.
+           MOVE FUNCTION CURRENT-DATE(7:2) TO WS-TODAY-DAY
+           STRING WS-TODAY-MONTH "-" WS-TODAY-DAY
+           INTO WS-TODAY
+           END-STRING.
+
+           CALL "customer-filterer" USING WS-TODAY.
 
