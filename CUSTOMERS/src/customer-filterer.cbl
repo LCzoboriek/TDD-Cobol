fe31aaa86242a64b178b2e1dcff43bcd33781203
Luke <lc169782@protonmail.com>
2021-05-28 15:29:37 +0100

    Stuck on file not open, customers.dat

--- CUSTOMERS/src/customer-filterer.cbl
+++ CUSTOMERS/src/customer-filterer.cbl
@@ -4,9 +4,11 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
            SELECT F-CUSTOMER-FILE ASSIGN TO 'customers.dat' 
-           ORGANIZATION IS LINE SEQUENTIAL.
+               ORGANIZATION IS LINE SEQUENTIAL.
            SELECT F-CARDS-FILE ASSIGN TO 'cards.dat'
-           ORGANIZATION IS LINE sequential.
+               ORGANIZATION IS LINE SEQUENTIAL.
+           SELECT F-TAXDAY-FILE ASSIGN TO 'cards-tax-day.dat'
+               ORGANIZATION IS LINE SEQUENTIAL.
        DATA DIVISION.
        FILE SECTION.
            FD F-CUSTOMER-FILE.
@@ -14,7 +16,7 @@
                05 PERSON-NAME PIC X(40).
                05 PERSON-ADDRESS PIC X(100).
                05 PERSON-DOB.
-                   10 DOB-YEAR PIC X(4).
+                   10 DOB-YEAR PIC 9(4).
                    10 YEAR-MON-SEPERATOR PIC X.
                    10 DOB-MON-DAY.
                        15 DOB-MON PIC 9(2).
@@ -26,19 +28,60 @@
                05 CARD-PERSON-NAME PIC X(40).
                05 CARD-PERSON-ADDRESS PIC X(100).
                05 CARD-GREETING PIC X(56).
-
+           FD F-TAXDAY-FILE.
+           01 TAX-CARDS-PERSON.
+               05 TAX-CARDS-PERSON-NAME PIC X(40).
+               05 TAX-CARDS-PERSON-ADDRESS PIC X(100).
+               05 TAX-CARDS-GREETING PIC X(56).
        WORKING-STORAGE SECTION.
            01 WS-FILE-IS-END PIC 9.
+           01 WS-DATE PIC 9(8).
        LINKAGE SECTION.
            01 LS-TODAY.
-               05 TODAY-MON PIC 9(2).
-               05 MON-DAY-SEPERATOR PIC X.
-               05 TODAY-DAY PIC 9(2).
+               05 LS-TODAY-MON PIC 9(2).
+               05 LS-MON-DAY-SEPERATOR PIC X.
+               05 LS-TODAY-DAY PIC 9(2).
+           01 LS-TODAY-YEAR PIC 9999.
+           
 
-       PROCEDURE DIVISION USING LS-TODAY.
+       PROCEDURE DIVISION USING LS-TODAY, LS-TODAY-YEAR.
            MOVE 0 TO WS-FILE-IS-END.
            OPEN INPUT F-CUSTOMER-FILE.
            OPEN EXTEND F-CARDS-FILE.
+           OPEN EXTEND F-TAXDAY-FILE.
+           
+
+           IF LS-TODAY = "04-06"
+               OPEN EXTEND F-TAXDAY-FILE
+               PERFORM TAX-DAY
+           END-IF.
+           PERFORM BIRTHDAY.
+           
+
+           TAX-DAY SECTION.
+          
+           PERFORM UNTIL WS-FILE-IS-END = 1
+           READ F-CUSTOMER-FILE
+               NOT AT END
+                   IF LS-TODAY-YEAR - DOB-YEAR >= 18 AND
+                   DOB-MON >= LS-TODAY-MON AND
+                   DOB-DAY >= LS-TODAY-DAY
+                       MOVE PERSON-NAME TO TAX-CARDS-PERSON-NAME
+                       MOVE PERSON-ADDRESS TO TAX-CARDS-PERSON-ADDRESS
+                       STRING "Happy Tax Day, " PERSON-NAME
+                       INTO TAX-CARDS-GREETING
+                       END-STRING
+                       WRITE TAX-CARDS-PERSON
+                       END-WRITE
+                    END-IF
+                  AT END
+                   MOVE 1 TO WS-FILE-IS-END
+           END-READ
+           END-PERFORM.
+                   
+                   
+
+           BIRTHDAY SECTION.
            PERFORM UNTIL WS-FILE-IS-END = 1
              READ F-CUSTOMER-FILE
                  NOT AT END
@@ -57,3 +100,4 @@
            END-PERFORM.
            CLOSE F-CUSTOMER-FILE.
            CLOSE F-CARDS-FILE.
+           CLOSE F-TAXDAY-FILE.
--- CUSTOMERS/test/test-customer-filterer.cbl
+++ CUSTOMERS/test/test-customer-filterer.cbl
@@ -1,5 +1,7 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. test-customer-filterer.
        PROCEDURE DIVISION.
-           CALL 'customer-filterer' USING '03-05'.
-       
+           CALL 'customer-filterer' USING '03-05', '2021'.
+
+           SET ENVIRONMENT "cards_dat" TO "cards-tax-day.dat".
+           CALL "customer-filterer" USING "04-06", "2021". 
